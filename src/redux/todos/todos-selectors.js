596406695b438c39b208e4a12572874cb90a22df
IrinaChoulgan <irina_choulgan@icloud.com>
2021-12-04 18:55:29 +0200

    + reselect

--- src/redux/todos/todos-selectors.js
+++ src/redux/todos/todos-selectors.js
@@ -1,12 +1,23 @@
+import { createSelector } from "reselect";
+
 export const getTodos = state => state.todos.items;
 export const getFilter = state => state.todos.filter;
 
-export const getVisibleTodos = state => {
-    const todos = getTodos(state);
-    const filter = getFilter(state);
-    const normalizedFilter = filter.toLowerCase();
+// export const getVisibleTodos = state => {
+//     const todos = getTodos(state);
+//     const filter = getFilter(state);
+//     const normalizedFilter = filter.toLowerCase();
   
+//     return todos.filter(({ text }) =>
+//       text.toLowerCase().includes(normalizedFilter),
+//     );
+//   };
+
+export const getVisibleTodos = createSelector([getTodos, getFilter],
+  (todos, filter)=> {
+
+    const normalizedFilter = filter.toLowerCase();
     return todos.filter(({ text }) =>
-      text.toLowerCase().includes(normalizedFilter),
-    );
-  };
+      text.toLowerCase().includes(normalizedFilter), 
+      );
+  })
